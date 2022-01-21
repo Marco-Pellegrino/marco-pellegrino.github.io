b9babaf2adeb90fb20299008891c038ff97c61be
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-01-21 18:08:56 +0000

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,9 +27,7 @@ function init() {
   document.body.append(renderer.domElement);
 
   
-  window.addEventListener('resize', function()
-
-    {
+  window.addEventListener('resize', function(){
       var width = window.innerWidth;
       var height = window.innerHeight;
       renderer.setSize(width, height);
