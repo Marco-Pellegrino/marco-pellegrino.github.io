a36be99415c4aaf6b9b6a0dc865d8dd000e2064c
MP <53620289+Marco-Pellegrino@users.noreply.github.com>
2022-02-06 14:28:16 +0000

    alpaca website

---
+++ assets/js/footer_alpaca4d.js
@@ -0,0 +1,25 @@
+document.getElementById("footer").innerHTML =
+`
+<footer class="page-footer green lighten-2">
+<div class="container">
+  <div class="row">
+    <div class="col s8">
+      <h5 class="white-text">Footer Content</h5>
+      <p  class="white-text">You can use rows and columns here to organize your footer content.</p>
+    </div>
+    <div class="col s4">
+      <h5 class="white-text">Connect</h5>
+      <i> <a  class="material-icons white-text" href="mailto:pellegrino.marco@icloud.com">mail</a> </i>
+      <i> <a  class="material-icons white-text" href="https://wa.me/393291536801">whatsapp</a> </i>
+      <i> <a  class="material-icons white-text" href="https://www.linkedin.com/in/mp0110/">work</a> </i>
+    </div>
+  </div>
+</div>
+
+<div class="footer-copyright">
+  <div class="container white-text">
+    Made with <a class= "orange-text" href="https://en.wikipedia.org/wiki/Love" target="_blank"> Love</a>
+  </div>
+</div>
+</footer>
+`
---
+++ assets/js/navbar_alpaca4d.js
@@ -0,0 +1,29 @@
+document.getElementById("navbar").innerHTML = 
+`
+<div class="navbar-fixed">
+    
+    <nav class="red lighten-1">
+      <div class="container">
+      <div class="nav-wrapper white">
+        <!-- <div class="container "> -->
+          <a class="sidenav-trigger" href="#" data-target="mobile-demo"><i class="material-icons white-text">menu</i></a>
+          <a class="brand-logo white-text" href="index.html">Alpaca4d</a>       
+          <ul class="right hide-on-med-and-down">
+            <li><a class="white-text" href="about.html">About</a></li>
+            <li><a class="white-text" href="download.html">Download</a></li>
+            <li><a class="white-text" href="examples.html">Examples</a></li>
+          </ul>
+      </div>
+      </div>
+    </nav>
+  
+  </div>
+  
+  
+    <ul class="sidenav  indigo darken-2" id="mobile-demo">
+      <li><a class="white-text" href="about.html">About</a></li>
+      <li><a class="white-text" href="download.html">Download</a></li>
+      <li><a class="white-text" href="examples.html">Examples</a></li>
+    </ul>
+
+`
